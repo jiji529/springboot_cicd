3a25dfb669cece9d42bfaf8ea8898a480960a3b5
LimChangMin <cml039465@gmail.com>
2023-10-24 09:48:16 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -27,7 +27,7 @@ module.exports = {
       }
       , '/javaApi': {
         target: (process.env.VUE_APP_USE_SERVE_JAVA === "true" ? 
-            process.env.VUE_APP_JAVA_SERVE_HOST + '/' + process.env.VUE_APP_PHP_SERVE_CONTEXT :
+            process.env.VUE_APP_JAVA_SERVE_HOST + '/' + process.env.VUE_APP_JAVA_SERVE_CONTEXT :
             "http://127.0.0.1:" + process.env.VUE_APP_JPORT + '/' + process.env.VUE_APP_JAVA_SERVE_CONTEXT), // premium로 수정
         changeOrigin: true,
         logLevel: 'debug',
